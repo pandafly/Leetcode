9e30b16acd129c230cadb46503025dd0639502fc
pandafly <380960137@qq.com>
2016-05-10 00:02:18 -0500

    remove dup sorted array I

--- java/Round1/src/RmDupSortedArray.java
+++ java/Round1/src/RmDupSortedArray.java
@@ -1,4 +1,27 @@
-
+//import java.util.Arrays;
+class Solution{
+	public int removeDuplicates(int[] nums) {
+        int length = nums.length;
+        if(length==0) 
+        	return 0;
+        int count = 1;
+        for(int i=1; i<length; i++){
+        	if(nums[i]==nums[i-1]){
+        		continue;
+        	} else {
+        		nums[count]=nums[i];
+        		count++;
+        	}
+        }
+        return count;
+    }
+}
 public class RmDupSortedArray {
-
+	public static void main(String[] args) {
+		int[] a = {1, 1, 2, 2, 2, 3, 4, 5};
+		Solution s1 = new Solution();
+		int res = s1.removeDuplicates(a);
+		System.out.println("Result is: "+res);
+		//System.out.println("array is: "+Arrays.toString(a));
+	}
 }
